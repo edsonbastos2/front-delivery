3ad4f7d63e38a6b6dac485c026fd3cb52764a405
Edson Bastos <edsonbastos2@hotmail.com>
2024-05-01 11:42:07 -0300

    feat:add promo banner to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -10,6 +11,18 @@ export default function Home() {
         <Search />
       </div>
       <CategoryList />
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner01.png"
+          alt="desconto em pizza"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
     </>
   );
 }
