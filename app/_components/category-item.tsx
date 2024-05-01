3518c70ed90d3fd93d7dedd975c01811e0e6722c
Edson Bastos <edsonbastos2@hotmail.com>
2024-05-01 11:24:34 -0300

    feat:add category item and list component

---
+++ app/_components/category-item.tsx
@@ -0,0 +1,22 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  return (
+    <div className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-8 py-3 shadow-md">
+      <Image
+        src={category.imageUrl}
+        alt={category.name}
+        height={30}
+        width={30}
+      />
+      <span className="text-sm font-semibold">{category.name}</span>
+    </div>
+  );
+};
+
+export default CategoryItem;
--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -1,5 +1,15 @@
-const CategoryList = () => {
-  return <></>;
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+  return (
+    <div className="flex gap-4 overflow-scroll px-5 pt-6">
+      {categories.map((cat) => (
+        <CategoryItem category={cat} key={cat.id} />
+      ))}
+    </div>
+  );
 };
 
 export default CategoryList;
