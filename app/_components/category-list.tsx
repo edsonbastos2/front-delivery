35ba9ea29e7f207bbb4078a333ef3b0a116706cf
Edson Bastos <edsonbastos2@hotmail.com>
2024-05-01 20:00:25 -0300

    feat:add product list and product item components

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -4,7 +4,7 @@ import CategoryItem from "./category-item";
 const CategoryList = async () => {
   const categories = await db.category.findMany({});
   return (
-    <div className="flex gap-4 overflow-x-scroll px-5 pt-6">
+    <div className="flex gap-4 overflow-x-scroll px-5 pt-6 [&::-webkit-scrollbar]:hidden">
       {categories.map((cat) => (
         <CategoryItem category={cat} key={cat.id} />
       ))}
